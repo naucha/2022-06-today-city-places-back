6863f37fa26a6ef51453e95e77b7315658fd1de0
ignasi reixach urcola <ignasi@MacBookRu.local>
2022-07-13 17:51:38 +0200

    Add register router

--- src/server/index.js
+++ src/server/index.js
@@ -4,6 +4,7 @@ const { default: helmet } = require("helmet");
 const cors = require("cors");
 const locationRouter = require("./routers/locationRouter");
 const { corsOptions } = require("../utils/cors");
+const userRouter = require("./routers/userRouter");
 
 const app = express();
 
@@ -13,5 +14,5 @@ app.use(express.json());
 app.use(helmet({ crossOriginResourcePolicy: false }));
 
 app.use("/locations", locationRouter);
-
+app.use("/user", userRouter);
 module.exports = app;
