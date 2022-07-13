57af03069552a63e216e3f61c7b1c4c409abb3c7
ignasi reixach urcola <ignasi@MacBookRu.local>
2022-07-13 10:29:18 +0200

    Add cors to handle a cross orogin resource from netlify and localhost

--- src/server/index.js
+++ src/server/index.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const morgan = require("morgan");
+const { default: helmet } = require("helmet");
+const cors = require("cors");
 const locationRouter = require("./routers/locationRouter");
+const { corsOptions } = require("../utils/cors");
 
 const app = express();
 
+app.use(cors(corsOptions));
 app.use(morgan("dev"));
 app.use(express.json());
+app.use(helmet({ crossOriginResourcePolicy: false }));
 
-app.use("/location", locationRouter);
+app.use("/locations", locationRouter);
 
 module.exports = app;
---
+++ src/utils/cors.js
@@ -0,0 +1,11 @@
+const corsOptions = {
+  origin: [
+    "http://localhost:3000",
+    "http://localhost:3001",
+    "http://localhost:3002",
+    "https://today-city-places.netlify.app",
+    "https://today-city-places.netlify.app/",
+  ],
+};
+
+module.exports = { corsOptions };
