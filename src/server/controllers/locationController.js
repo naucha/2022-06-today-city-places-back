f459d914fed30ab170ef10785675f374534d03a2
ignasi reixach urcola <ignasi@MacBookRu.local>
2022-07-21 12:14:10 +0200

    Find locations by userId and implement authorization to validate user

--- src/server/controllers/locationController.js
+++ src/server/controllers/locationController.js
@@ -3,14 +3,28 @@ const debug = require("debug")(
   "today-city-places:server:controllers:locationController"
 );
 const chalk = require("chalk");
+const jwt = require("jsonwebtoken");
 const Location = require("../../database/models/Location");
+const User = require("../../database/models/User");
 
 const getLocations = async (req, res, next) => {
+  const { authorization } = req.headers;
+
   try {
-    const location = await Location.find({});
+    if (!authorization.includes("Bearer ")) {
+      throw new Error("Not bearer");
+    }
+    const token = authorization.replace("Bearer ", "");
+    const userData = jwt.verify(token, process.env.JWT_SECRET);
+
+    const { myplaces } = await User.findById(userData).populate({
+      path: "myplaces",
+      model: Location,
+    });
+
     debug(chalk.bgBlue("Getting locations"));
 
-    res.status(200).json(location);
+    res.status(200).json(myplaces);
   } catch (error) {
     const userError = new Error();
     userError.statusCode = 404;
